218a9379059bfc94c23fc35ba6f214ab4e7eda23
ApenasJoy <joy.programando@gmail.com>
2026-05-26 10:13:37 +0000

    Completei o desafio Nível Mestre!

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -15,8 +15,9 @@ int main() {
     char *topo[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G','H', 'I', 'J'};
 
     //Apresentando o nome do jogo
-    printf("     Batalha Naval!\n");
+    printf("     Batalha Naval!\n\n");
 
+    printf("Posiciando os navios...\n");
     //Aplicando um array de A a J para representar o topo do tabuleiro
     printf("   ");
     for (int i = 0; i < 10; i++) {
@@ -44,31 +45,52 @@ int main() {
     printf("\n");
     }
     
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
     // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
     // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    //Apresentando o nome do jogo
+    printf("\nAtacando as embarcações...\n");
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    //Aplicando, mais uma vez, um array de A a J para representar o topo do tabuleiro
+    printf("   ");
+    for (int i = 0; i < 10; i++) {
+        printf("%c ", topo[i]);
+    }
+    printf("\n");
+
+    //Exibindo o tabuleiro
+    for (int i = 0; i < 10; i++) {
+        //Aplicando, novamente, uma coluna enumerada de 1 a 10 para representar a lateral do tabuleiro
+        printf("%d ", i + 1);
+
+        for (int j = 0; j < 10; j++) {
+            //Trabalhandom, desta vez, com estruturas if e else para posicionar os ataque
+            if (
+            //Ataque em formato de cone
+            (i == 0 && j == 3) || 
+            (i == 1 && j >= 2 && j <= 4) || 
+            (i == 2 && j >= 1 && j <= 5) ||
+            //Ataque em formato de cruz
+            (i == 3 && j == 8) || 
+            (i == 4 && j >= 7 && j <= 9) || 
+            (i == 5 && j == 8) ||
+            //Aqtaque em formato de octaedro
+            (i == 4 && j == 3) ||
+            (i == 5 && j >= 2 && j <= 4) ||  
+            (i == 6 && j >= 1 && j <= 5) ||
+            (i == 7 && j >= 2 && j <= 4) ||  
+            (i == 8 && j == 3)) {
+                
+                tabuleiro[i][j] = 3; 
+            } else {
+                tabuleiro[i][j] = 0;
+            }
+            printf(" %d", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
